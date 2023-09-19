9f0fd9332953d07cbb366396939cad5a31982119
Cole Anam <coleanam2000@gmail.com>
2023-09-19 09:04:39 -0400

    Initial Commit

---
+++ Control Fun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+//  Created by Cole Anam on 19/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
